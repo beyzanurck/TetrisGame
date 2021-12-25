f76f7ad3ab023032cb31f23a7fa603bd5c551dc8
Beyzanur Kılınç <nurbeyzakilinc@gmail.com>
2021-12-25 16:32:16 -0600

    restart

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 
 namespace tetris
 {
@@ -11,6 +12,71 @@ namespace tetris
             Console.CursorVisible = false;
 
             drawFrame();
+            int x = 80;
+            int y = 3;
+
+            int exY, exX;
+
+            int[,] oldShapes = new int[4, 4];
+            int[,] newShapes = new int[4, 4];
+
+            int orient = 1;
+            while (true)
+            {
+                int timer = 1000;
+
+                putShape(2, orient, x, y, newShapes);
+
+                exX = x;
+                exY = y;
+
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                    while (Console.KeyAvailable)
+                    {
+                        keyInfo = Console.ReadKey();
+                    }
+
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+
+                            orient++;
+
+                            break;
+
+                        case ConsoleKey.RightArrow:
+
+                            x = x + 2;
+
+                            break;
+
+                        case ConsoleKey.LeftArrow:
+
+                            x = x - 2;
+
+                            break;
+
+                        case ConsoleKey.DownArrow:
+
+                            timer = 100;
+
+                            break;
+                    }
+                }
+
+                if (y <= 36)
+                {
+                    y = y + 1;
+                }
+
+                Thread.Sleep(timer);
+
+            }
+           
 
             Console.ReadLine();
         }
@@ -157,5 +223,86 @@ namespace tetris
 
 
         }
+
+        static void putShape(int id, int orient, int x, int y, int[,] oldShape)
+        {
+            if (id == 1)
+            {
+                oldShape = new int[,] { { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+
+            if (id == 2)
+            {
+                if (orient % 2 != 0)
+                {
+                    oldShape = new int[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 1, 1, 1, 1 } };
+
+                }
+                if (orient % 2 == 0)
+                {
+                    oldShape = new int[,] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } };
+
+                }
+            }
+
+            if (id == 3)
+            {
+                oldShape = new int[,] { { 0, 1, 1, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+            if (id == 4)
+            {
+                oldShape = new int[,] { { 1, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+            if (id == 5)
+            {
+                oldShape = new int[,] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+            if (id == 6)
+            {
+                oldShape = new int[,] { { 0, 1, 0, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+            if (id == 7)
+            {
+                oldShape = new int[,] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.SetCursorPosition(x,y);
+                    if (oldShape[i, j] == 1)
+                    {
+                        Console.Write("██");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    x = x + 2;
+                }
+                y = y + 1;
+                x = x - 8;
+            }
+        }
+
+        static void eraseShape(int id, int orient, int x, int y, int[,] oldShape)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    if (oldShape[i, j] == 1)
+                    {
+                        Console.Write("  ");
+                    }
+                    x = x + 2;
+                }
+                y = y + 1;
+                x = x - 8;
+            }
+        }
     }
+    
 }
