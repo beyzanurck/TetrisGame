597c6cb0fbdba856ff7dd654cc84f12f4c79f203
Beyzanur Kılınç <nurbeyzakilinc@gmail.com>
2021-12-25 16:39:25 -0600

    restart

--- Program.cs
+++ Program.cs
@@ -12,106 +12,50 @@ namespace tetris
             Console.CursorVisible = false;
 
             drawFrame();
-            int x = 80;
-            int y = 3;
 
-            int exY, exX;
-
-            int[,] oldShapes = new int[4, 4];
-            int[,] newShapes = new int[4, 4];
-
-            int orient = 1;
-            while (true)
-            {
-                int timer = 1000;
-
-                putShape(2, orient, x, y, newShapes);
-
-                exX = x;
-                exY = y;
+            Random random = new Random();
+            int shapeId = random.Next(1, 8);
 
 
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+            drawShape(5, 0, 61, 3);
+            drawShape(5, 1, 68, 3);
+            drawShape(5, 2, 74, 3);
+            drawShape(5, 3, 81, 3);
 
-                    while (Console.KeyAvailable)
-                    {
-                        keyInfo = Console.ReadKey();
-                    }
-
-                    switch (keyInfo.Key)
-                    {
-                        case ConsoleKey.UpArrow:
-
-                            orient++;
-
-                            break;
-
-                        case ConsoleKey.RightArrow:
-
-                            x = x + 2;
-
-                            break;
-
-                        case ConsoleKey.LeftArrow:
-
-                            x = x - 2;
-
-                            break;
-
-                        case ConsoleKey.DownArrow:
-
-                            timer = 100;
-
-                            break;
-                    }
-                }
-
-                if (y <= 36)
-                {
-                    y = y + 1;
-                }
-
-                Thread.Sleep(timer);
-
-            }
-           
 
             Console.ReadLine();
         }
 
         static void drawFrame()
         {
-
-            for (int y = 2; y < 42; y++)
+            for (int y = 2; y <= 23; y++)
             {
-                for (int x = 60; x < 100; x++)
+                for (int x = 60; x <= 81; x++)
                 {
-                    Console.SetCursorPosition(x, y);                                       
+                    Console.SetCursorPosition(x, y);
 
                     if (x == 60 && y == 2)
                     {
                         Console.Write("╔");
                     }
-                    if (x == 60 && y == 41)
+                    if (x == 60 && y == 23)
                     {
                         Console.Write("╚");
                     }
-                    if (x == 99 && y == 2)
+                    if (x == 81 && y == 2)
                     {
                         Console.Write("╗");
                     }
-                    if (x == 99 && y == 41)
+                    if (x == 81 && y == 23)
                     {
                         Console.Write("╝");
                     }
 
-                    if ((x == 60 || x == 99) && (y > 2 && y < 41))
+                    if ((x == 60 || x == 81) && (y > 2 && y < 23))
                     {
                         Console.Write("║");
                     }
-                    else if ((y == 2 || y == 41) && (x < 99))
+                    else if ((y == 2 || y == 23) && (x < 81))
                     {
                         Console.Write("═");
                     }
@@ -123,157 +67,65 @@ namespace tetris
 
                 Console.WriteLine();
             }
+        }
+        static void drawShape(int id, int rot, int x, int y)
+        {
+            ConsoleColor color = new ConsoleColor();
+            int[,] shape = new int[4, 4];
 
-            for (int y = 10; y < 20; y++)
+            if (id == 1) //sarı
             {
-                for (int x = 110; x < 130; x++)
-                {
-                    Console.SetCursorPosition(x, y);                                       
-
-                    if (x == 110 && y == 10)
-                    {
-                        Console.Write("╔");
-                    }
-                    if (x == 110 && y == 19)
-                    {
-                        Console.Write("╚");
-                    }
-                    if (x == 129 && y == 10)
-                    {
-                        Console.Write("╗");
-                    }
-                    if (x == 129 && y == 19)
-                    {
-                        Console.Write("╝");
-                    }
-
-                    if ((x == 110 || x == 129) && (y > 10 && y < 19))
-                    {
-                        Console.Write("║");
-                    }
-                    else if ((y == 10 || y == 19) && (x < 129))
-                    {
-                        Console.Write("═");
-                    }
-                    else
-                    {
-                        Console.WriteLine(" ");
-                    }
-                }
-
-                Console.WriteLine();
+                color = ConsoleColor.Yellow;
+                shape = new int[,] { { 1, 1 }, { 1, 1 } };
+            }
+            if (id == 2) // turkuaz
+            {
+                color = ConsoleColor.DarkCyan;
+                shape = new int[,] { { 1, 1, 1, 1 } };
+            }
+            if (id == 3) // yeşil
+            {
+                color = ConsoleColor.Green;
+                shape = new int[,] { { 0, 1, 1 }, { 1, 1, 0 } };
+            }
+            if (id == 4) //kırmızı
+            {
+                color = ConsoleColor.DarkRed;
+                shape = new int[,] { { 1, 1, 0 }, { 0, 1, 1 } };
+            }
+            if (id == 5) // mor
+            {
+                color = ConsoleColor.Magenta;
+                shape = new int[,] { { 0, 1, 0 }, { 1, 1, 1 } };
+            }
+            if (id == 6) // mavi
+            {
+                color = ConsoleColor.DarkBlue;
+                shape = new int[,] { { 1, 0, 0 }, { 1, 1, 1 } };
+            }
+            if (id == 7) //turuncu
+            {
+                color = ConsoleColor.DarkYellow;
+                shape = new int[,] { { 0, 0, 1 }, { 1, 1, 1 } };
             }
 
-            for (int y = 10; y < 26; y++)
+            for (int i = 0; i < rot; i++)
             {
-                for (int x = 30; x < 50; x++)
+                shape = rotate(shape);
+            }
+
+            int r = shape.GetLength(0);
+            int c = shape.GetLength(1);
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
                 {
                     Console.SetCursorPosition(x, y);
-
-                    if (x == 30 && y == 10)
+                    if (shape[i, j] == 1)
                     {
-                        Console.Write("╔");
-                    }
-                    if (x == 30 && y == 25)
-                    {
-                        Console.Write("╚");
-                    }
-                    if (x == 49 && y == 10)
-                    {
-                        Console.Write("╗");
-                    }
-                    if (x == 49 && y == 25)
-                    {
-                        Console.Write("╝");
-                    }
-
-                    if ((x == 30 || x == 49) && (y > 10 && y < 25))
-                    {
-                        Console.Write("║");
-                    }
-                    else if ((y == 10 || y == 25) && (x < 49))
-                    {
-                        Console.Write("═");
-                    }
-                    else if (y == 15 || y == 20)
-                    {
-                        Console.Write("═");
-                    }
-                    else
-                    {
-                        Console.WriteLine(" ");
-                    }
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.SetCursorPosition(77,1);
-            Console.WriteLine("TETRIS");
-
-            Console.SetCursorPosition(118,11);
-            Console.WriteLine("NEXT");
-
-            Console.SetCursorPosition(38, 11);
-            Console.WriteLine("SCORE"); 
-            Console.SetCursorPosition(38, 16);
-            Console.WriteLine("LEVEL"); 
-            Console.SetCursorPosition(38, 21);
-            Console.WriteLine("LINE");
-
-
-        }
-
-        static void putShape(int id, int orient, int x, int y, int[,] oldShape)
-        {
-            if (id == 1)
-            {
-                oldShape = new int[,] { { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
-            }
-
-            if (id == 2)
-            {
-                if (orient % 2 != 0)
-                {
-                    oldShape = new int[,] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 1, 1, 1, 1 } };
-
-                }
-                if (orient % 2 == 0)
-                {
-                    oldShape = new int[,] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } };
-
-                }
-            }
-
-            if (id == 3)
-            {
-                oldShape = new int[,] { { 0, 1, 1, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
-            }
-            if (id == 4)
-            {
-                oldShape = new int[,] { { 1, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
-            }
-            if (id == 5)
-            {
-                oldShape = new int[,] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
-            }
-            if (id == 6)
-            {
-                oldShape = new int[,] { { 0, 1, 0, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
-            }
-            if (id == 7)
-            {
-                oldShape = new int[,] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.SetCursorPosition(x,y);
-                    if (oldShape[i, j] == 1)
-                    {
+                        Console.ForegroundColor = color;
                         Console.Write("██");
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                     else
                     {
@@ -281,28 +133,28 @@ namespace tetris
                     }
                     x = x + 2;
                 }
+                x = x - c * 2;
                 y = y + 1;
-                x = x - 8;
             }
+
         }
 
-        static void eraseShape(int id, int orient, int x, int y, int[,] oldShape)
+        static int[,] rotate(int[,] matris)
         {
-            for (int i = 0; i < 4; i++)
+            int[,] tr = new int[matris.GetLength(1), matris.GetLength(0)];
+
+            for (int c = 0; c < matris.GetLength(1); c++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int r = 0; r < matris.GetLength(0); r++)
                 {
-                    Console.SetCursorPosition(x, y);
-                    if (oldShape[i, j] == 1)
-                    {
-                        Console.Write("  ");
-                    }
-                    x = x + 2;
+                    tr[c, matris.GetLength(0) - r - 1] = matris[r, c];
                 }
-                y = y + 1;
-                x = x - 8;
             }
+
+            return tr;
+
         }
+
     }
-    
+
 }
